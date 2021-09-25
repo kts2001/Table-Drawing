70644eb4d7ce9e586c7885f931371c92fe98b9e4
Kareem Zahra <kts2001@live.com>
2021-09-25 18:15:23 -0400

    Final Version

---
+++ assignment2.js
@@ -0,0 +1,24 @@
+﻿function createTable() {
+    var numberOfRows = document.getElementById('numberofrows').value;
+
+    var numberOfCols = document.getElementById('numberofcols').value;
+
+    var tableHeader = '<table border="1">\n';
+    var tableBody = '';
+
+    for (var i = 1; i < numberOfRows; i++) {
+        tableBody = tableBody + '<tr>';
+        for (var j = 1; j < numberOfCols; j++) {
+            tableBody = tableBody + '<td>';
+            tableBody = tableBody + '' + i + ',' + j;
+            tableBody = tableBody + '</td>'
+        }
+        tableBody = tableBody + '</tr>\n';
+    }
+    var tableFooter = '</table>';
+
+    document.getElementById('result').innerHTML = tableHeader + tableBody + tableFooter;
+
+}
+
+
